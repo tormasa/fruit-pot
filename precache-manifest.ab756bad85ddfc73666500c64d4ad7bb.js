51a34235f5a747b20e51948fa738493e9f84ed0a
Matti <matti.torvinen@gmail.com>
2020-05-24 12:38:18 +0300

    Updates

---
+++ precache-manifest.ab756bad85ddfc73666500c64d4ad7bb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c5b704abf57029d2a1a1b0177668f59",
+    "url": "/fruit-pot/index.html"
+  },
+  {
+    "revision": "22296233ef4e785e43e8",
+    "url": "/fruit-pot/static/css/main.79df3d86.chunk.css"
+  },
+  {
+    "revision": "82185d6a94e9d8cca90b",
+    "url": "/fruit-pot/static/js/2.9c49996e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fruit-pot/static/js/2.9c49996e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22296233ef4e785e43e8",
+    "url": "/fruit-pot/static/js/main.dde1b67a.chunk.js"
+  },
+  {
+    "revision": "f5c68f0c0dbd9496cea8",
+    "url": "/fruit-pot/static/js/runtime-main.5229fac6.js"
+  },
+  {
+    "revision": "a44df0211d21862762fb38d08c512a59",
+    "url": "/fruit-pot/static/media/apple.a44df021.png"
+  },
+  {
+    "revision": "52a0005dcefe30937d2f997cabcd784b",
+    "url": "/fruit-pot/static/media/banana.52a0005d.png"
+  },
+  {
+    "revision": "11104fbe41e44fa701c8e5dcf59709ef",
+    "url": "/fruit-pot/static/media/blueberry.11104fbe.png"
+  },
+  {
+    "revision": "efe875d5c132e248036bb44aada737c7",
+    "url": "/fruit-pot/static/media/cherry.efe875d5.png"
+  },
+  {
+    "revision": "36b238a06154431a5f65b6f68f706c68",
+    "url": "/fruit-pot/static/media/lemon.36b238a0.png"
+  },
+  {
+    "revision": "0db872dd8441ae8d2c5dbf7ea6e59f2c",
+    "url": "/fruit-pot/static/media/pear.0db872dd.png"
+  },
+  {
+    "revision": "edf3dfd8e324293e6af31c24f5aa3b25",
+    "url": "/fruit-pot/static/media/star.edf3dfd8.png"
+  },
+  {
+    "revision": "751a51a48e57d381fa8e77a9e66afebc",
+    "url": "/fruit-pot/static/media/strawberry.751a51a4.png"
+  }
+]);
